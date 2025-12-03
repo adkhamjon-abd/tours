580beaa2df49a6b1bd63c5634d5a5f90dd8c61c7
Hp <adkhamjon.abd@gmail.com>
2025-12-03 17:53:03 +0500

    Add an API getUserIdFromToken to UserController to find users by their Token

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -43,4 +43,8 @@ public class UserController {
     public String getAll(){
         return userRepository.findAll();
     }
+    // get UserId using token
+    public Integer getUserIdFromToken(String token) {
+        return sessions.get(token);
+    }
 }
