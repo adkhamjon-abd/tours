f06d7b4ad184ac31ab0992088358415925ca076f
Hp <adkhamjon.abd@gmail.com>
2025-12-03 16:31:32 +0500

    Create User model, repository and controller classes

---
+++ src/main/java/org/example/controller/UserController.java
@@ -0,0 +1,46 @@
+package org.example.controller;
+
+import org.example.model.User;
+import org.example.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    private final Map<String, Integer> sessions = new HashMap<>();
+
+    public UserController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/login")
+    public Map<String, String> login(@RequestBody User login){
+        User user = userRepository.findByUsername(login.getUsername());
+        Map<String, String> response = new HashMap<>();
+
+        if (user != null && user.getPassword().equals(login.getPassword())){
+            String token = "token" + user.getId();
+            sessions.put(token, user.getId());
+            response.put("token", token);
+        }else {
+            response.put("error", "Invalid username data");
+        }
+        return response;
+    }
+
+    @PostMapping("/register")
+    public String createUser(@RequestBody User user){
+        return userRepository.save(user);
+    };
+
+    @GetMapping
+    public String getAll(){
+        return userRepository.findAll();
+    }
+}
---
+++ src/main/java/org/example/repository/UserRepository.java
@@ -0,0 +1,51 @@
+package org.example.repository;
+
+import org.example.model.User;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class UserRepository {
+    private Map<Integer, User> users = new HashMap<>();
+    private int nextId = 1;
+    public UserRepository(){
+        save(new User(0, "admin", "admin"));
+    }
+
+    public String save(User user){
+        for (User existing : users.values()) {
+            if (existing.getUsername().equals(user.getUsername())) {
+                return "Username already exists. Please choose another.";
+            }
+        }
+        user.setId(nextId);
+        users.put(nextId, user);
+        nextId++;
+        return "User: " + user.getUsername() + " created with id: " + user.getId();
+    }
+
+    public User findByUsername(String userName){
+        User user = users.values().stream()
+                .filter(user1 -> user1.getUsername().equals(userName))
+                .toList().get(0);
+
+        return user;
+    }
+
+    public String findAll() {
+        String result = "";
+        for (User user : users.values()) {
+            String username = user.getUsername();
+            String password = user.getPassword();
+
+            // Corrected concatenation and newline handling
+            result += " UserName: " + username + " Password:" + password + "\n";
+        }
+        return result;
+    }
+
+
+}
