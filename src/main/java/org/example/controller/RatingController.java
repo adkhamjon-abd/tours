c1b5a0f18387d17b2ef17073b68e5ae2b79ab525
Hp <adkhamjon.abd@gmail.com>
2025-12-04 14:10:42 +0500

    Create Service class for Rating model

--- src/main/java/org/example/controller/RatingController.java
+++ src/main/java/org/example/controller/RatingController.java
@@ -2,30 +2,32 @@ package org.example.controller;
 
 import org.example.model.Rating;
 import org.example.repository.RatingRepository;
+import org.example.service.RatingService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/ratings")
 public class RatingController {
     private final RatingRepository ratingRepository;
-    public RatingController(RatingRepository ratingRepository){
+    private final RatingService ratingService;
+    public RatingController(RatingRepository ratingRepository, RatingService ratingService){
         this.ratingRepository = ratingRepository;
+        this.ratingService =  ratingService;
     }
 
     @PostMapping()
     public String createRating(@RequestBody Rating rating){
-        return ratingRepository.save(rating);
-
+        return ratingService.createRating(rating);
     }
 
     @GetMapping()
     public String getAll(){
-        return ratingRepository.findAll();
+        return ratingService.getAll();
     }
 
     @GetMapping("/{id}")
     public String getRatingByTourId(@PathVariable("id") int id){
-        return ratingRepository.fingAverageRatingByTourId(id);
+        return ratingService.getRatingByTourId(id);
     }
 
 }
---
+++ src/main/java/org/example/service/RatingService.java
@@ -0,0 +1,28 @@
+package org.example.service;
+
+import org.example.model.Rating;
+import org.example.repository.RatingRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RatingService {
+
+    private RatingRepository ratingRepository;
+
+    public RatingService(RatingRepository ratingRepository){
+        this.ratingRepository = ratingRepository;
+    }
+
+
+    public String createRating(Rating rating) {
+        return ratingRepository.save(rating);
+    }
+
+    public String getAll() {
+        return ratingRepository.findAll();
+    }
+
+    public String getRatingByTourId(int id) {
+        return ratingRepository.fingAverageRatingByTourId(id);
+    }
+}
