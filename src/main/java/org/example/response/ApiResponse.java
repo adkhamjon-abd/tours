8174e824edf0a7e01d9e0d4261ee4928ec8a2d27
Hp <adkhamjon.abd@gmail.com>
2025-12-12 10:12:19 +0500

    Refactor: create reponse wrapper for consistency

---
+++ src/main/java/org/example/response/ApiResponse.java
@@ -0,0 +1,45 @@
+package org.example.response;
+
+public class ApiResponse<T> {
+
+    boolean success;
+    T data;
+    String error;
+
+    public ApiResponse(){}
+
+    public ApiResponse(T data){
+        this.success = true;
+        this.data = data;
+    }
+
+    public ApiResponse(String error){
+        this.success = false;
+        this.error = error;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+}
