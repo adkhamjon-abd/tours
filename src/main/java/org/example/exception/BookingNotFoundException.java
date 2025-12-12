039740463933d019c617e8cdb132072889216b19
Hp <adkhamjon.abd@gmail.com>
2025-12-12 10:44:41 +0500

    Add custom exception BookingNotFound and global error handler

---
+++ src/main/java/org/example/exception/BookingNotFoundException.java
@@ -0,0 +1,8 @@
+package org.example.exception;
+
+public class BookingNotFoundException extends RuntimeException{
+
+    public BookingNotFoundException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/org/example/exception/GlolbalExceptionHandler.java
@@ -0,0 +1,17 @@
+package org.example.exception;
+
+import org.example.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlolbalExceptionHandler {
+
+    @ExceptionHandler(BookingNotFoundException.class)
+    public ResponseEntity<ApiResponse<String>> bookingNotFoundException(BookingNotFoundException bookingNotFoundException){
+        ApiResponse<String> response = new ApiResponse<>(bookingNotFoundException.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+}
