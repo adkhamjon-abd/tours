02518f178d9626093f53b2e354c3d9ced9d2d434
Hp <adkhamjon.abd@gmail.com>
2025-12-30 11:12:33 +0500

    refactor: replace in-memory UserRepository with Hibernate persistence

--- src/main/java/org/example/repository/UserRepository.java
+++ src/main/java/org/example/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package org.example.repository;
 
+import org.example.config.HibernateUtil;
 import org.example.model.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
@@ -10,36 +13,107 @@ public class UserRepository{
     private Map<Integer, User> users = new HashMap<>();
     private int nextId = 2;
 
+//    public UserRepository(){
+//        users.put(1, new User(1, "admin", "admin"));
+//    }
     public UserRepository(){
-        users.put(1, new User(1, "admin", "admin"));
+
     }
 
     public User save(User user){
-
-        user.setId(nextId++);
-        users.put(user.getId(), user);
+        Session session = null;
+        Transaction tx = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            tx = session.beginTransaction();
+            session.persist(user);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            throw e;
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
         return user;
     }
 
     public List<User> findAll() {
-        return new ArrayList<>(users.values());
+        Session session = null;
+
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            return session.createQuery("FROM User", User.class).list();
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
     }
 
     public Optional<User> findById(int id){
-        User user = users.values().stream()
-                .filter(u -> u.getId() == id)
-                .findFirst()
-                .orElse(null);
-        return Optional.ofNullable(user);
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return Optional.ofNullable(session.find(User.class, id));
+        }
+
+//        User user = users.values().stream()
+//                .filter(u -> u.getId() == id)
+//                .findFirst()
+//                .orElse(null);
+//        return Optional.ofNullable(user);
     }
 
 
     public void deleteById(int id) {
+        Session session = null;
+        Transaction tx = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            tx = session.beginTransaction();
+            session.createQuery(
+                            "DELETE FROM User u WHERE u.id = :id")
+                    .setParameter("id", id)
+                    .executeUpdate();
+            tx.commit();
+
+        }catch (Exception e) {
+            if (tx != null) tx.rollback();
+            throw e;
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
         users.entrySet().removeIf(entry -> entry.getValue().getId() == id);
+
     }
 
     public User update(User existingUser) {
-        users.replace(existingUser.getId(), existingUser);
-        return existingUser;
+        Session session = null;
+        Transaction tx = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            tx = session.beginTransaction();
+
+            User user = session.find(User.class, existingUser.getId());
+            if (user != null) {
+                user.setId(existingUser.getId());
+                user.setUsername(existingUser.getUsername());
+                user.setPassword(existingUser.getPassword());
+            }
+
+            tx.commit();
+            return user;
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            throw e;
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
     }
 }
