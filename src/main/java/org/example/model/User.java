fe06dd9b38c89710c620ad671d418b5e5b585ed3
Hp <adkhamjon.abd@gmail.com>
2025-12-30 11:12:13 +0500

    Add annotations to User entity

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,8 +1,14 @@
 package org.example.model;
 
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String username;
     private String password;
