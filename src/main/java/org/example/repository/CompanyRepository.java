cbfca4050c1945d24e3291658feee616d9a14956
Hp <adkhamjon.abd@gmail.com>
2026-01-05 17:46:40 +0500

    Refactor: Use injected SessionFactory to open session in CompanyRepository

--- src/main/java/org/example/repository/CompanyRepository.java
+++ src/main/java/org/example/repository/CompanyRepository.java
@@ -1,9 +1,8 @@
 package org.example.repository;
 
-import org.example.config.HibernateUtil;
 import org.example.model.Company;
-import org.example.model.User;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
@@ -12,21 +11,18 @@ import java.util.*;
 @Repository
 public class CompanyRepository {
 
-    private final Map<Integer, Company> companies = new HashMap<>();
-    private int nextId = 6;
-    public CompanyRepository(){
-        companies.put(1, new Company(1, "Green Travel"));
-        companies.put(2, new Company(2, "World Tours"));
-        companies.put(3, new Company(3, "Samo Travel"));
-        companies.put(4, new Company(4, "Holiday Abroad"));
-        companies.put(5, new Company(5, "Afsona Travel"));
+    private final SessionFactory sessionFactory;
+
+    public CompanyRepository(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
-    public Company save(Company company){
+
+    public Company save(Company company) {
 
         Session session = null;
         Transaction tx = null;
         try {
-            session = HibernateUtil.getSessionFactory().openSession();
+            session = sessionFactory.openSession();
             tx = session.beginTransaction();
             session.persist(company);
             tx.commit();
@@ -43,8 +39,8 @@ public class CompanyRepository {
         }
     }
 
-    public Optional<Company> findById(int id){
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+    public Optional<Company> findById(int id) {
+        try (Session session = sessionFactory.openSession()) {
             return Optional.ofNullable(session.find(Company.class, id));
         }
     }
@@ -53,7 +49,7 @@ public class CompanyRepository {
         Session session = null;
 
         try {
-            session = HibernateUtil.getSessionFactory().openSession();
+            session = sessionFactory.openSession();
             return session.createQuery("FROM Company", Company.class).list();
         } finally {
             if (session != null && session.isOpen()) {
@@ -66,7 +62,7 @@ public class CompanyRepository {
         Session session = null;
         Transaction tx = null;
         try {
-            session = HibernateUtil.getSessionFactory().openSession();
+            session = sessionFactory.openSession();
             tx = session.beginTransaction();
             session.createQuery(
                             "DELETE FROM Rating r WHERE r.id = :id")
@@ -74,7 +70,7 @@ public class CompanyRepository {
                     .executeUpdate();
             tx.commit();
 
-        }catch (Exception e) {
+        } catch (Exception e) {
             if (tx != null) tx.rollback();
             throw e;
         } finally {
@@ -88,7 +84,7 @@ public class CompanyRepository {
         Session session = null;
         Transaction tx = null;
         try {
-            session = HibernateUtil.getSessionFactory().openSession();
+            session = sessionFactory.openSession();
             tx = session.beginTransaction();
 
             Company company = session.find(Company.class, existing.getId());
