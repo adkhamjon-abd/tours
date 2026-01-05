ea43d1b0f925c6cb0902b738c714e88f65eaeabb
Hp <adkhamjon.abd@gmail.com>
2026-01-05 11:05:55 +0500

    Refactor: tidy up RatingController

--- src/main/java/org/example/controller/RatingController.java
+++ src/main/java/org/example/controller/RatingController.java
@@ -13,24 +13,25 @@ import java.util.List;
 @RequestMapping("/ratings")
 public class RatingController {
     private final RatingServiceImpl ratingService;
-    public RatingController(RatingServiceImpl ratingService){
-        this.ratingService =  ratingService;
+
+    public RatingController(RatingServiceImpl ratingService) {
+        this.ratingService = ratingService;
     }
 
     @PostMapping()
-    public ResponseEntity<ApiResponse<Rating>> createRating(@RequestBody Rating rating){
+    public ResponseEntity<ApiResponse<Rating>> createRating(@RequestBody Rating rating) {
         Rating newRating = ratingService.createRating(rating);
         return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>(newRating));
     }
 
     @GetMapping()
-    public ResponseEntity<ApiResponse<List<Rating>>> getAll(){
+    public ResponseEntity<ApiResponse<List<Rating>>> getAll() {
         List<Rating> ratings = ratingService.getAll();
         return ResponseEntity.ok(new ApiResponse<>(ratings));
     }
 
     @GetMapping("/tours/{id}")
-    public ResponseEntity<ApiResponse<Double>> getAverageRatingByTourId(@PathVariable("id") int id){
+    public ResponseEntity<ApiResponse<Double>> getAverageRatingByTourId(@PathVariable("id") int id) {
         Double average = ratingService.getAverageRatingByTourId(id);
         return ResponseEntity.ok(new ApiResponse<>(average));
     }
@@ -48,13 +49,13 @@ public class RatingController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ApiResponse<Rating>> updateRating(@PathVariable("id") int id, @RequestBody Rating updateRating){
+    public ResponseEntity<ApiResponse<Rating>> updateRating(@PathVariable("id") int id, @RequestBody Rating updateRating) {
         Rating rating = ratingService.updateRating(id, updateRating);
         return ResponseEntity.ok(new ApiResponse<>(rating));
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<ApiResponse<Rating>> patchRating(@PathVariable("id") int id, @RequestBody Rating patchRating){
+    public ResponseEntity<ApiResponse<Rating>> patchRating(@PathVariable("id") int id, @RequestBody Rating patchRating) {
         Rating rating = ratingService.patchRating(id, patchRating);
         return ResponseEntity.ok(new ApiResponse<>(rating));
     }
